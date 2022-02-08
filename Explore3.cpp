c66a15ca7d40a69a3463ffcb04189a3da6bc1fbe
YASHARORA6 <>
2022-02-08 17:26:12 +0000

    Explore3 working

--- Explore3.cpp
+++ Explore3.cpp
@@ -26,12 +26,10 @@ int main() {
 	cout << "Creating a at 3, 15" << endl;
 	Point a(3,5);
 	cout << "Creating b with default" << endl;
-	Point b;
+	Point b(10,33);
 
 	cout << "Point a: x = " << a.x << ", y = " << a.y << endl;
 	cout << "Point b: x = " << b.x << ", y = " << b.y << endl;
 
 	return 0;
 }
-
-
